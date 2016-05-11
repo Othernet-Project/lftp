e8063c434ebf90b791c7d1ebc2415f2a8915f022
Manish Mathai <manishpmathai@gmail.com>
2016-05-11 19:37:09 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    """ Return content of specified file """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+VERSION = '0.0.1'
+
+setup(
+    name='lftp',
+    version=VERSION,
+    license='GPLv3',
+    packages=find_packages(),
+    include_package_data=True,
+    long_description=read('README.rst'),
+    install_requires=[
+        'pyftpdlib'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Applicaton',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
